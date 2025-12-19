870230c46f3f52b168ae0f446198074ca04a087c
Nicolas Dupont <npg.dupont@gmail.com>
2025-12-19 14:50:45 +0100

    Update deployment

---
+++ deployment/iroco/pm2.config.cjs
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  apps: [
+    {
+      name: "ota-contribution-tool-iroco",
+      script: 'npm',
+      args: 'start',
+      cwd: '/home/debian/' + path.basename(__dirname),
+      env: {
+        NODE_ENV: 'production'
+      },
+      min_uptime: '10s',
+      max_restarts: 10,
+      restart_delay: 1000,
+      exponential_backoff_restart_delay: true,
+      log_date_format: "YYYY-MM-DDTHH:mm:ssZ"
+    }
+  ],
+};
